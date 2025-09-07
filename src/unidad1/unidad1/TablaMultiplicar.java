761c04ce735da0ca8fc5f7c32ee4324e6dda1eea
Skarik-L <slbarbosab@ufpso.edu.co>
2025-09-07 15:54:44 -0500

    Add files via upload

---
+++ src/unidad1/unidad1/TablaMultiplicar.java
@@ -0,0 +1,22 @@
+package unidad1;
+// programa que genere la tabla de multiplicar de un número ingresado por el usuario utilizando un ciclo for
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+         
+        // ingresar el #
+        System.out.println("Ingresar un número: ");
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= 9 ; i++){
+            int producto = num * i;
+            
+
+            System.out.println( i + "x" + num + "=" + producto);
+        }
+
+    }
+}
